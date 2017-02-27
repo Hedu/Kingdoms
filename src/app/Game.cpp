9dc82239c4ee9d2d92220dda9d86fd6aee8aa44f
hedu <eperezghedu@gmail.com>
2017-02-27 21:40:24 +0100

    integrate OGRE: apply changes

--- src/app/Game.cpp
+++ src/app/Game.cpp
@@ -5,15 +5,18 @@
  *      Author: hedu
  */
 
-#include "../logic/entities/Entity.h"
-#include "../../test/logic/entities/components/TestComponent.h"
+#include "Application.h"
+#include "../graphics/GraphicManager.h"
+#include "../graphics/Scene.h"
 #include "../logic/GameManager.h"
+#include "../logic/entities/Entity.h"
+#include "../logic/entities/components/GraphicComponent.h"
+#include "../../test/logic/entities/components/TestComponent.h"
 
 #include <ctime>
 #include <iostream>
 
 //fakeMain includes
-#include "Application.h"
 
 #define MILLIS_PER_FRAME (float)1000/60
 
@@ -25,16 +28,25 @@ void realMain() {
 	float previousTime = getCurrentMillis();
 	float diff = previousTime;
 
+	Application app;
+	app.init();
+
+	GraphicManager *graphicManager = GraphicManager::getInstance();
+	graphicManager->addScene("main", new Scene(app.getRoot(), app.getWindow()));
+
 	GameManager *gm = GameManager::getInstance();
 	Entity *e = new Entity();
-	Component *cp = new TestComponent();
+	Component *cp = new TestComponent(e);
+	Component *gcp = new GraphicComponent(e, "objeto1", "ninja.mesh");
 
 	e->addComponent(cp);
+	e->addComponent(gcp);
 	gm->addEntity(e);
 
 	gm->broadcastMessage(new Message(NULL));
 
 	gm->init();
+	app.go();
 	while (true) {
 		diff = getCurrentMillis() - previousTime;
 		if (diff > MILLIS_PER_FRAME) {
@@ -45,8 +57,7 @@ void realMain() {
 }
 
 void fakeMain() {
-	Application app;
-	app.go();
+	std::cout << "Fake Main" << std::endl;
 }
 
 int main() {
